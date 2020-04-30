74157590ae3e7345317d302f6c1be5741e0fdfe7
kuki <bkko@outlook.kr>
2020-04-30 21:49:39 +0900

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import LeftPane from './components/LeftPane';
+import RightPane from './components/RightPane';
+import {SampleProvider} from './contexts/sample';
+import {AnotherProvider} from './contexts/another';
+import Counter from './components/Counter';
 
-function App() {
+const AppProvider = ( {contexts, children}) => contexts.reduce(
+  (prev, context) => React.createElement(context, {
+    children: prev
+  }),
+  children
+)
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppProvider contexts={[SampleProvider, AnotherProvider]}>
+      <div className="panes">
+        <LeftPane />
+        <RightPane />
+      </div>
+      <Counter/>
+    </AppProvider>
   );
-}
+};
 
 export default App;
